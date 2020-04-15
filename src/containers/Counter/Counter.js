26fa44155567b76e03a30af74931b1825c963c67
ngirimana <chadrackngirimana@gmail.com>
2020-04-15 15:21:28 +0200

    Updating-State-Immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -5,7 +5,8 @@ import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
     state = {
-        counter: 0
+        counter: 0,
+        results:[]
     }
 
     counterChangedHandler = (action, value) => {
@@ -36,6 +37,15 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={ this.props.onDecrementCounter } />
                 <CounterControl label="Add 10" clicked={ this.props.onAddCounter } />
                 <CounterControl label="Subtract - 15" clicked={ this.props.onSubtractCounter } />
+                <hr/>
+                <button onClick={this.props.onStoreResult}>Store Ressult</button>
+                <ul>
+                    {this.props.storedResults.map(strResult=>(
+                        <li key={strResult.id}onClick={this.props.onDeleteResult}>{strResult.value}</li>
+                    ))}
+                    
+                    
+                </ul>
             </div>
         );
     }
@@ -43,7 +53,8 @@ class Counter extends Component {
 // add mapStateToProps
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults:state.results
     };
 };
 const mapDispatchToProps = dispatch => {
@@ -51,7 +62,9 @@ const mapDispatchToProps = dispatch => {
         onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
         onAddCounter: () => dispatch({ type: 'ADD',val:10 }),
         onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
-        onSubtractCounter: () => dispatch({ type: 'SUBSTRACT',val:15 })
+        onSubtractCounter: () => dispatch({ type: 'SUBSTRACT',val:15 }),
+        onStoreResult:()=>dispatch({type:'STORE_RESULT'}),
+        onDeleteResult:()=>dispatch({type:'DELETE_RESULT'})
     }
 }
 
